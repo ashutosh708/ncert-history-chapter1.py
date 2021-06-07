b16a92f1449fc237bab5658770b84bf4d38d8328
ashutosh708 <85440783+ashutosh708@users.noreply.github.com>
2021-06-07 10:03:38 +0530

    Add files via upload

---
+++ History chapter-French Revolution.py
@@ -0,0 +1,118 @@
+print("welcome to history chapter 'FRENCH REVOLUTION'")
+print("NOTE 1:- DO NOT WRITE ANY TYPE SIGNS e.g.-QUESTION MARK/FULL STOP etc.")
+print("NOTE 2:-WRITE QUESTION IN THE FORM: e.g-what is______")
+print("")
+print("")
+b=True
+while b==True:
+    a=input("Enter your question related to this chapter").lower()
+    print("")
+
+    if a=='who was louis xvi':
+        print("")
+        print("Louis XVI was the king of france from 1774-1789")
+            
+    elif a=='who became the king of france in 1774':
+        print("")
+        print("Louis XVI")
+
+    elif a=='who was wife of Louis XVI':
+         print("Austrian princess Marie Antoinette")
+
+    elif a=='why louis xvi found empty treasury':
+        print("")
+        print("because of long years of war")
+
+    elif a=='why state was forced to increase taxes':
+        print("")
+        print("To maintain the army,court and government offices state was forced to increase taxes")
+
+    elif a=='what is old regime':
+        print("")
+        print("The society of france before 1789 is considered as old regime")
+
+    elif a=='how french society was divided':
+        print("French society was divided into three groups:")
+        print("")
+        print("1.Clergy--Preist in church are clergy.")
+        print("2.Nobility-Royal people,kings,queens etc.")
+        print("3.Third Estate- Buisnessmen,Merchants,peasants, artisans,labours,servants")
+
+
+    elif a=='what is clergy':
+        print("Preist in church are clergy.")
+
+    elif a=='what is nobility':
+        print("Royal people,kings,queens etc.")
+
+    elif a=='what is third estate':
+        print("")
+        print("Big Buisnessmen,Merchants,peasants, artisans,labours,servants")
+
+    elif a=='who owns 60% of land in france':
+        print("")
+        print("people of 1st and 2nd estate including richer members of third estate")
+
+    elif a=='which estate has to pay taxes':
+        print("")
+        print("3rd estate")
+
+    elif a=='does the people of 1st and 2nd estate paid taxes':
+        print ("")
+        print("No, only 3rd estate paid taxes")
+
+    elif a=='how many types of taxes were there':
+        print("")
+        print("there were two types of taxes")
+        print("1.Tithes Tax--A tax collected by the church comprising one-tenth of agricultural produce")
+        print("2.Taille Tax-- tax to be paid directly to the state")
+
+    elif a=='what is tithes tax':
+        print("A tax collected by the church comprising one-tenth of agricultural produce")
+
+    elif a=='what is taille tax':
+        print(" tax to be paid directly to the state")
+    
+    elif a=='what was the population of france in 1715':
+        print("23 million")
+
+    elif a=='what was the population of france in 1789':
+        print("28 million")
+
+
+    elif a=='when was the population of france was 23 million':
+        print("1715")
+
+
+    elif a=='when was the population of france was 28 million':
+        print("1789")
+
+
+    elif a=='when the population of france rose from 23 million to 28 million':
+        print("between 1715-1789")
+
+    elif a=='what led to rapid increase in demand of foodgrains':
+        print("")
+        print("The increase in population led to rapid increase in demand of food grains")
+
+    elif a=='why price of bread increased':
+        print("")
+        print("because of insufficient production and high population")
+
+    elif a=='what is subsistence crisis':
+        print("")
+        print("A situation in which basic need of livelihood are endangered is called subsistence crisis")
+
+    else:
+        print("result not found")
+
+
+    c=input("Do you want to repeat the program:yes/no").lower()
+    if c=='yes':
+        continue
+    else:
+        exit()
+
+
+   
+
